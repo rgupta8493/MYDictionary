93f60087009b560616b0e30a72113351306a511d
rgupta8493 <r.gupta8493@ufl.edu>
2015-12-12 19:25:24 -0500

    Initial commit

---
+++ app/build/generated/source/buildConfig/release/com/word/dictionary/BuildConfig.java
@@ -0,0 +1,13 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.word.dictionary;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = false;
+  public static final String APPLICATION_ID = "com.word.dictionary";
+  public static final String BUILD_TYPE = "release";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "";
+}
